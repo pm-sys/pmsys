3dd0369e24ea2799273ecc4d1f942175366cadee
daviddl <david.delisle.lalancette@gmail.com>
2017-12-01 11:12:00 -0500

    Add a registration number to Patient, and allow searching for that field
    I added a new automatically incremented field for registration number, and made sure to allow admins to search for it in the Patient list.
    I also added it as the first thing on a list item.

--- registrations/admin.py
+++ registrations/admin.py
@@ -3,8 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Patient, Appointment, Ward, MedicalStaff
 
-admin.site.register(Patient)
 admin.site.register(MedicalStaff)
 admin.site.register(Appointment)
 admin.site.register(Ward)
 
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('registration_number', 'last_name', 'first_name')
+    search_fields = ['registration_number']
+
+
+admin.site.register(Patient, PatientAdmin)
