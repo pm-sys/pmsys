e6434f8c43891f6b24321e92a662e14ae40a6644
Kevin Le <kevin.kent.le@gmail.com>
2017-12-02 11:16:58 -0500

    deleted unused views

--- registrations/views.py
+++ registrations/views.py
@@ -8,14 +8,3 @@ from .models import Patient
 
 def index(request):
     return redirect('/admin/')
-
-def detail(request, patient_id):
-    patient = get_object_or_404(Patient, pk=patient_id)
-    return render(request, 'registrations/detail.html', {'patient': patient})
-
-def results(request, patient_id):
-    response = "You're looking at the results of patient %s."
-    return HttpResponse(response % patient_id)
-
-def update(request, patient_id):
-    return HttpResponse("You're updating patient %s." % patient_id)
