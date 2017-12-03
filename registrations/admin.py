17afb290effd35fe95c9e8a9c992a8204e0263a9
Kevin Le <kevin.kent.le@gmail.com>
2017-12-03 11:21:54 -0500

    fix some bugs that prevented it from running

--- registrations/admin.py
+++ registrations/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Patient, Appointment, Ward, MedicalStaff
+from .models import Patient, Appointment, Ward, MedicalStaff, Prescription
 
 admin.site.register(MedicalStaff)
 admin.site.register(Appointment)
